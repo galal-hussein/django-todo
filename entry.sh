7ce02234f732fabb8142b7fce6fe9a4dd5274828
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2025-11-05 16:37:09 +0200

    add dockerfile and convert to mysql
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>

---
+++ entry.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -x
+set +e
+
+python manage.py makemigrations
+
+python manage.py migrate
+
+python manage.py createsuperuser --noinput
+
+python manage.py runserver 0.0.0.0:8000
--- todoApp/settings.py
+++ todoApp/settings.py
@@ -75,9 +75,20 @@ WSGI_APPLICATION = 'todoApp.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE', 'todoapp'),
+        'USER': os.environ.get('MYSQL_USER', 'todo'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD', 'todopass'),
+        'HOST': os.environ.get('MYSQL_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('MYSQL_PORT', '3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
